6449f6f1f2a3032369bc50947247781c4cdaf3d7
Daniel Finger <64750813+DanielZFinger@users.noreply.github.com>
2023-09-01 12:39:42 -0400

    Update lambda.py
    updated lambda.py

--- lambda.py
+++ lambda.py
@@ -4,16 +4,120 @@ import uuid
 
 dynamodb_client = boto3.resource('dynamodb')
 table = dynamodb_client.Table('OrderTable')
+brandTable = dynamodb_client.Table('BrandTable')
+modelTable = dynamodb_client.Table('ModelTable')
 
 def lambda_handler(event, context):
-    operation = event['operation']
+    print(json.dumps(event))
+    body = json.loads(event['body'])
+    operation = body['operation']
+    # operation = event['operation']
+    
+    # *****|All Model Functions|*****
+    if operation == 'retrieveModels':
+        try:
+            # response = table.get_item(Key={'CustomerID': customer_id})
+            # order = response.get('Item')
+            response = modelTable.scan(
+            )
+            
+            models = response.get('Items', [])
+            if models:
+                mResponse = "Models succesfully retrieved!"
+                response = {
+                    "statusCode": 200,
+                    "headers": {
+                        "Access-Control-Allow-Origin": "*",
+                        "Access-Control-Allow-Headers": "*",
+                        "Content-Type": "application/json",
+                        "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+                    },
+                "body": json.dumps(models)
+                }
+                return response
+            else:
+                mResponse = "Models failed to retrieve!"
+                response = {
+                    "statusCode": 403,
+                    "headers": {
+                        "Access-Control-Allow-Origin": "*",
+                        "Access-Control-Allow-Headers": "*",
+                        "Content-Type": "application/json",
+                        "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+                    },
+                "body": json.dumps(mResponse)
+                }
+                return response
+        except Exception as e:
+            mResponse = "Model failed to update!"
+            response = {
+                "statusCode": 402,
+                "headers": {
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Headers": "*",
+                    "Content-Type": "application/json",
+                    "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+                },
+            "body": json.dumps(mResponse)
+            }
+            return response
+    
+    # *****|All Brand Functions|*****
+    if operation == 'retrieveBrand':
+        try:
+            # response = table.get_item(Key={'CustomerID': customer_id})
+            # order = response.get('Item')
+            response = brandTable.scan(
+            )
+            
+            brands = response.get('Items', [])
+            if brands:
+                mResponse = "Brands succesfully retrieved!"
+                response = {
+                    "statusCode": 200,
+                    "headers": {
+                        "Access-Control-Allow-Origin": "*",
+                        "Access-Control-Allow-Headers": "*",
+                        "Content-Type": "application/json",
+                        "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+                    },
+                "body": json.dumps(brands)
+                }
+                return response
+            else:
+                mResponse = "Brands failed to retrieve!"
+                response = {
+                    "statusCode": 400,
+                    "headers": {
+                        "Access-Control-Allow-Origin": "*",
+                        "Access-Control-Allow-Headers": "*",
+                        "Content-Type": "application/json",
+                        "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+                    },
+                "body": json.dumps(mResponse)
+                }
+                return response
+        except Exception as e:
+            mResponse = "Brand failed to update!"
+            response = {
+                "statusCode": 400,
+                "headers": {
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Headers": "*",
+                    "Content-Type": "application/json",
+                    "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+                },
+            "body": json.dumps(mResponse)
+            }
+            return response
+        
     
     
+    # *****|All Order Functions|*****
+        
     # update a specific orders contents based on its orderID
     if operation == 'update':
-        order_id = event['OrderID']
-        employee_id = event['EmployeeID']
-        customer_id = event['CustomerID']
+        employee_id = body['EmployeeID']
         
         try:
             messageResponse = table.update_item(
@@ -27,7 +131,7 @@ def lambda_handler(event, context):
                 "headers": {
                     "Access-Control-Allow-Origin": "*",
                     "Access-Control-Allow-Headers": "*",
-                    "Content-Type": "*/*",
+                    "Content-Type": "application/json",
                     "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
                 },
             "body": json.dumps(mResponse)
@@ -40,19 +144,21 @@ def lambda_handler(event, context):
                 "headers": {
                     "Access-Control-Allow-Origin": "*",
                     "Access-Control-Allow-Headers": "*",
-                    "Content-Type": "*/*",
+                    "Content-Type": "application/json",
                     "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
                 },
             "body": json.dumps(mResponse)
             }
-            return {response}
+            return response
     
     # create an order
     elif operation == 'create':
         # Extract order details from the event
         # add new order params here when wishing to expan table variables
-        employee_id = event['EmployeeID']
-        customer_id = event['CustomerID']
+        employee_id = body['EmployeeID']
+        customer_id = body['CustomerID']
+        brand = body['Brand']
+        model = body['Model']
         
         # Create a new order in the table
         try:
@@ -63,7 +169,9 @@ def lambda_handler(event, context):
                 Item={
                     'OrderID': order_id,
                     'EmployeeID': employee_id,
-                    'CustomerID': customer_id
+                    'CustomerID': customer_id,
+                    'Brand': brand,
+                    'Model': model,
                 }
             )
             mResponse = "Succesfully created order!"
@@ -72,7 +180,7 @@ def lambda_handler(event, context):
                 "headers": {
                     "Access-Control-Allow-Origin": "*",
                     "Access-Control-Allow-Headers": "*",
-                    "Content-Type": "*/*",
+                    "Content-Type": "application/json",
                     "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
                 },
             "body": json.dumps(mResponse)}
@@ -84,32 +192,41 @@ def lambda_handler(event, context):
                 "headers": {
                     "Access-Control-Allow-Origin": "*",
                     "Access-Control-Allow-Headers": "*",
-                    "Content-Type": "*/*",
+                    "Content-Type": "application/json",
                     "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
                 },
             "body": json.dumps(mResponse)
             }
-            return {response}
+            return response
     
     # retrieve info regarding a specific order
-    elif operation == 'retrieve':
-        order_id = event['OrderID']
-        employee_id = event['EmployeeID']
-        
+    elif operation == 'retrieveAll':
+        customer_id = body['CustomerID']
+
         try:
-            response = table.get_item(Key={'OrderID': order_id, 'EmployeeID': employee_id})
-            order = response.get('Item')
+            # response = table.get_item(Key={'CustomerID': customer_id})
+            # order = response.get('Item')
+            response = table.scan(
+                FilterExpression='CustomerID = :cid',
+                ExpressionAttributeValues={':cid': customer_id}
+            )
+            
+            order = response.get('Items', [])
+            print(order)
             if order:
+                print("yayayayya")
                 mResponse = "Order succesfully retrieved!"
                 response = {
                     "statusCode": 200,
                     "headers": {
                         "Access-Control-Allow-Origin": "*",
                         "Access-Control-Allow-Headers": "*",
-                        "Content-Type": "*/*",
+                        "Content-Type": "application/json",
                         "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
                     },
-                "body": json.dumps(mResponse)}
+                "body": json.dumps(order)
+                }
+                return response
             else:
                 mResponse = "Order failed to retrieve!"
                 response = {
@@ -117,12 +234,12 @@ def lambda_handler(event, context):
                     "headers": {
                         "Access-Control-Allow-Origin": "*",
                         "Access-Control-Allow-Headers": "*",
-                        "Content-Type": "*/*",
+                        "Content-Type": "application/json",
                         "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
                     },
                 "body": json.dumps(mResponse)
                 }
-                return {response}
+                return response
         except Exception as e:
             mResponse = "Order failed to update!"
             response = {
@@ -130,13 +247,12 @@ def lambda_handler(event, context):
                 "headers": {
                     "Access-Control-Allow-Origin": "*",
                     "Access-Control-Allow-Headers": "*",
-                    "Content-Type": "*/*",
+                    "Content-Type": "application/json",
                     "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
                 },
             "body": json.dumps(mResponse)
             }
-            return {response}
-    
+            return response
     else:
         mResponse = "Invalid operation!"
         response = {
@@ -144,10 +260,10 @@ def lambda_handler(event, context):
             "headers": {
                 "Access-Control-Allow-Origin": "*",
                 "Access-Control-Allow-Headers": "*",
-                "Content-Type": "*/*",
+                "Content-Type": "application/json",
                 "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
             },
         "body": json.dumps(mResponse)
         }
-        return {response}
+        return response
 
